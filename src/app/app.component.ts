c2fcb92364d9678156d56ed45939a2f14b75c456
Ryan Morris <ryan.morris@centro.net>
2020-03-07 08:16:27 -0500

    Add my estimate for BP-7962

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { map, takeWhile, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'estimate-BP7962';
+  public estimating: boolean;
+  public estimateTimer: null | Observable<number>;
+  public estimate: number | null;
+
+  constructor() {
+    this.estimating = false;
+    this.estimateTimer = null;
+    this.estimate = null;
+  }
+
+  startEstimating() {
+    this.estimating = true;
+    this.estimateTimer = timer(0, 1000)
+      .pipe(
+        map((n: number) => 3 - n),
+        tap((n: number) => {
+          if (n === 0) {
+            this.estimate = 2;
+          }
+        }),
+        takeWhile((n: number) => n > 0)
+      );
+  }
+  
 }
